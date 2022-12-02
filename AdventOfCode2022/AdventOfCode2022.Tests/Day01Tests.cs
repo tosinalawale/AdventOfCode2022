63ed2e20f1f864aaa32e513640d7d7b05a22d1e1
Tosin Alawale <talawale@hotmail.com>
2022-12-02 13:32:06 +0000

    Solve Day 1 Part 1

---
+++ AdventOfCode2022/AdventOfCode2022.Tests/Day01Tests.cs
@@ -0,0 +1,39 @@
+namespace AdventOfCode2022.Tests
+{
+    using AdventOfCode2022;
+    using FluentAssertions;
+
+    public class Day01Tests
+    {
+        [Test]
+        public void Part1_CalculateResultForSimpleExample()
+        {
+            var input = new[]
+            {
+                "1000",
+                "2000",
+                "3000",
+                "",
+                "4000",
+                "",
+                "5000",
+                "6000",
+                "",
+                "7000",
+                "8000",
+                "9000",
+                "",
+                "10000"
+            };
+
+            Day01.CalculateResultForPartOne(input).Should().Be(24000);
+        }
+
+        [Test]
+        public void Part1_CalculateResult()
+        {
+            var input = File.ReadAllLines(@"Input/input01.txt");
+            System.Console.WriteLine(Day01.CalculateResultForPartOne(input));
+        }
+    }
+}
---
+++ AdventOfCode2022/AdventOfCode2022/Day01.cs
@@ -0,0 +1,27 @@
+﻿namespace AdventOfCode2022
+{
+    using System;
+
+    public class Day01
+    {
+        public static int CalculateResultForPartOne(string[] input)
+        {
+            var currentSum = 0;
+            var elfCalorieTotals = new List<int>();
+
+            foreach (var line in input)
+            {
+                if (line.Equals(string.Empty))
+                {
+                    elfCalorieTotals.Add(currentSum);
+                    currentSum = 0;
+                    continue;
+                }
+
+                currentSum += int.Parse(line);
+            }
+
+            return elfCalorieTotals.Max();
+        }
+    }
+}
