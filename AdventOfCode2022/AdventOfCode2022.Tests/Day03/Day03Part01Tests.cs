68351023761cc06c34e97ab3df1ccc625a8831bd
Tosin Alawale <talawale@hotmail.com>
2022-12-03 07:36:38 +0000

    Solve day 3 part 1

---
+++ AdventOfCode2022/AdventOfCode2022.Tests/Day03/Day03Part01Tests.cs
@@ -0,0 +1,39 @@
+namespace AdventOfCode2022.Tests.Day03
+{
+    using AdventOfCode2022.Day03;
+
+    public class Day03Part01Tests
+    {
+        [TestCase("a",1)]
+        [TestCase("z",26)]
+        [TestCase("A",27)]
+        [TestCase("Z",52)]
+        public void CanConvertItemToItemPriority(string item, int expectedItemPriority) 
+        {
+            Day03Part01.ToItemPriority(item).Should().Be(expectedItemPriority);
+        }
+
+        [Test]
+        public void CalculateResultForSimpleExample()
+        {
+            var input = new[]
+            {
+                "vJrwpWtwJgWrhcsFMMfFFhFp",
+                "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+                "PmmdzqPrVvPwwTWBwg",
+                "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+                "ttgJtRGJQctTZtZT",
+                "CrZsJsPPZsGzwwsLwLmpwMDw"
+            };
+
+            Day03Part01.CalculateResult(input).Should().Be(157);
+        }
+
+        [Test]
+        public void CalculateResult()
+        {
+            var input = File.ReadAllLines(@"Input/input03.txt");
+            Console.WriteLine(Day03Part01.CalculateResult(input));
+        }
+    }
+}
---
+++ AdventOfCode2022/AdventOfCode2022/Day03/Day03Part01.cs
@@ -0,0 +1,29 @@
+﻿namespace AdventOfCode2022.Day03
+{
+    using System;
+
+    public class Day03Part01
+    {
+        public static int CalculateResult(string[] input)
+        {
+            return input.Select(r => FindItemInBothCompartments(r))
+                .Select(i => ToItemPriority(i))
+                .Sum();
+        }
+
+        public static int ToItemPriority(string item)
+        {
+            var asciiCode = (int)char.Parse(item);
+
+            return (asciiCode >= 97) ? asciiCode - 96 : asciiCode - 38;
+        }
+
+        private static string FindItemInBothCompartments(string rucksackContents)
+        {
+            var firstCompartment = rucksackContents.Substring(0, rucksackContents.Length / 2);
+            var secondCompartment = rucksackContents.Substring(rucksackContents.Length / 2);
+
+            return new string(firstCompartment.Intersect(secondCompartment).ToArray());
+        }
+    }
+}
