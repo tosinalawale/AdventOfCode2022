d14e95d591e600d046601a26cd44f3521864a68f
Tosin Alawale <talawale@hotmail.com>
2022-12-08 12:59:55 +0000

    Clean up day 7 part 1 tests

--- AdventOfCode2022/AdventOfCode2022.Tests/Day07/Day07Part01Tests.cs
+++ AdventOfCode2022/AdventOfCode2022.Tests/Day07/Day07Part01Tests.cs
@@ -5,7 +5,7 @@ namespace AdventOfCode2022.Tests.Day07
     public class Day07Part01Tests
     {
         [Test]
-        public void CanGetDirectorySizeForBaseCase()
+        public void CanGetTotalDirectorySizeForBaseCase()
         {
             var input = new[]
             {
@@ -20,7 +20,7 @@ namespace AdventOfCode2022.Tests.Day07
         }
 
         [Test]
-        public void CanGetDirectorySizeForMoreCaseWithNestedFolder()
+        public void CanGetTotalDirectorySizeForMoreCaseWithNestedFolder()
         {
             var input = new[]
             {
@@ -71,60 +71,6 @@ namespace AdventOfCode2022.Tests.Day07
 
             Day07Part01.GetRootDirectorySize(input).Should().Be(48381165);
         }
-
-        //        [Test]
-        //        public void CanBuildDirectoryTreeFromInput()
-        //        {
-        //            var input = new[]
-        //            {
-        //                "$ cd /",
-        //                "$ ls",
-        //                "dir a",
-        //                "14848514 b.txt",
-        //                "8504156 c.dat",
-        //                "dir d",
-        //                "$ cd a",
-        //                "$ ls",
-        //                "29116 f",
-        //                "$ cd d",
-        //                "$ ls",
-        //                "4060174 j"
-        //            };
-
-        //            var directoryA = new FileSystemElement
-        //            {
-        //                Name = "a",
-        //                Contents = new List<FileSystemElement>
-        //                {
-
-        //                }
-        //            };
-
-        //            var expectedFileSystemTree = new FileSystemElement
-        //            {
-        //                Name = "/",
-        //                Contents = new List<FileSystemElement>
-        //                {
-
-        //                }
-        //            };
-
-        ///*@"- / (dir)
-        //    - a(dir)
-        //    - e(dir)
-        //        - i(file, size = 584)
-        //    - f(file, size = 29116)
-        //    - g(file, size = 2557)
-        //    - h.lst(file, size = 62596)
-        //    - b.txt(file, size = 14848514)
-        //    - c.dat(file, size = 8504156)
-        //    - d(dir)
-        //    - j(file, size = 4060174)
-        //    - d.log(file, size = 8033020)
-        //    - d.ext(file, size = 5626152)
-        //    - k(file, size = 7214296)"*/
-
-        //        }
 
         [Test]
         public void CalculateResultForSimpleExample()
