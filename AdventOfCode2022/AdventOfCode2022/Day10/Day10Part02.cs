9c9acd4831169808a0de5529063b0567a3cb20cc
Tosin Alawale <talawale@hotmail.com>
2022-12-14 15:22:09 +0000

    Solve day 10 part 2

---
+++ AdventOfCode2022/AdventOfCode2022/Day10/Day10Part02.cs
@@ -0,0 +1,20 @@
+﻿namespace AdventOfCode2022.Day10
+{
+    using AdventOfCode2022.Tests.Day10;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Day10Part02
+    {
+        public static string CalculateResult(string[] input)
+        {
+            var program = new Program();
+            program.Execute(input);
+
+            return program.GenerateSpriteImage();
+        }
+    }
+}
--- AdventOfCode2022/AdventOfCode2022/Day10/Program.cs
+++ AdventOfCode2022/AdventOfCode2022/Day10/Program.cs
@@ -1,5 +1,7 @@
 ﻿namespace AdventOfCode2022.Tests.Day10
 {
+    using System.Text;
+
     public class Program
     {
         public Dictionary<int, int> RegisterValues { get; set; } = new Dictionary<int, int>();
@@ -25,6 +27,33 @@
                     currentCycle++;
                 }
             }
+        }
+
+        public string GenerateSpriteImage()
+        {
+            var stringBuilder = new StringBuilder();
+            var registerValueDuringCurrentCycle = 1;
+
+            //Remember - register value DURING a cycle is the value AFTER the last cycle,
+            //hence why I'm looping through the cycles from 0.
+            for (int currentCycle = 0; currentCycle < RegisterValues.Count; currentCycle++)
+            {
+                var currentPixelPosition = (currentCycle % 40);
+
+                var pixelToDraw = 
+                    (registerValueDuringCurrentCycle - 1 <= currentPixelPosition
+                    && currentPixelPosition <= registerValueDuringCurrentCycle + 1)
+                    ? "#"
+                    : ".";
+
+                stringBuilder.Append(pixelToDraw);
+
+                registerValueDuringCurrentCycle = RegisterValues[currentCycle + 1];
+
+                if (currentCycle % 40 == 39) stringBuilder.AppendLine();
+            }
+
+            return stringBuilder.ToString().TrimEnd();
         }
     }
 }
