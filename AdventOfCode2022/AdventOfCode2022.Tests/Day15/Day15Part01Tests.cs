5f4cc2363712105d1c26cb087a6087780ff8435d
Tosin Alawale <talawale@hotmail.com>
2022-12-28 16:27:44 +0000

    WIP - working solution that doesn't quite scale: It calculates all positions with no beacons for all sensors - works for simple example with few sensors but takes too long with full puzzle input

---
+++ AdventOfCode2022/AdventOfCode2022.Tests/Day15/Day15Part01Tests.cs
@@ -0,0 +1,58 @@
+namespace AdventOfCode2022.Tests.Day15
+{
+    using AdventOfCode2022.Day15;
+
+    public class Day15Part01Tests
+    {
+        [TestCase("Sensor at x=2, y=2: closest beacon is at x=4, y=5", 60)]
+        [TestCase("Sensor at x=8, y=7: closest beacon is at x=2, y=10", 180)]
+        public void CalculateNumberOfPositionsWithoutBeaconsForSingleSensor(string sensorInput, int expectedNumberOfPositions)
+        {
+            var input = new[]
+            {
+                sensorInput,
+            };
+
+            //for a manhattan distance of 5
+            //(4 + 4, 3 + 3, ... 1 + 1) * 4
+            //5 * 4
+            //(4 * 5/2)*4 + 5*4 = 60
+            // -1 for the nearest beacon's position (B) = 59
+            // +1 for the sensor's position = 60
+            //so formula is: n(n + 1)/2 * 4 + n*4
+
+            Day15Part01.GetNumberOfPositionsWithoutBeacons(input).Should().Be(expectedNumberOfPositions);
+        }
+
+        [Test]
+        public void CalculateResultForSimpleExample()
+        {
+            var input = new[]
+            {
+                "Sensor at x=2, y=18: closest beacon is at x=-2, y=15",
+                "Sensor at x=9, y=16: closest beacon is at x=10, y=16",
+                "Sensor at x=13, y=2: closest beacon is at x=15, y=3",
+                "Sensor at x=12, y=14: closest beacon is at x=10, y=16",
+                "Sensor at x=10, y=20: closest beacon is at x=10, y=16",
+                "Sensor at x=14, y=17: closest beacon is at x=10, y=16",
+                "Sensor at x=8, y=7: closest beacon is at x=2, y=10",
+                "Sensor at x=2, y=0: closest beacon is at x=2, y=10",
+                "Sensor at x=0, y=11: closest beacon is at x=2, y=10",
+                "Sensor at x=20, y=14: closest beacon is at x=25, y=17",
+                "Sensor at x=17, y=20: closest beacon is at x=21, y=22",
+                "Sensor at x=16, y=7: closest beacon is at x=15, y=3",
+                "Sensor at x=14, y=3: closest beacon is at x=15, y=3",
+                "Sensor at x=20, y=1: closest beacon is at x=15, y=3",
+            };
+
+            Day15Part01.CalculateResult(input).Should().Be(26);
+        }
+
+        [Test]
+        public void CalculateResult()
+        {
+            var input = File.ReadAllLines(@"Input/input15.txt");
+            Console.WriteLine(Day15Part01.CalculateResult(input));
+        }
+    }
+}
---
+++ AdventOfCode2022/AdventOfCode2022/Day15/Day15Part01.cs
@@ -0,0 +1,75 @@
+﻿namespace AdventOfCode2022.Day15
+{
+    public class Day15Part01
+    {
+        public static int CalculateResult(string[] input)
+        {
+            var grid = CreateGridFromInput(input);
+
+            return grid.Keys.Where(k => k.y == 10 && !grid[(k.x, k.y)].Equals('B')).Count();
+        }
+
+        private static Dictionary<(int x, int y), char> CreateGridFromInput(string[] input)
+        {
+            var grid = new Dictionary<(int x, int y), char>();
+
+            foreach (var line in input)
+            {
+                //"Sensor at x=2, y=18: closest beacon is at x=-2, y=15"
+                var lineParts = line.Split(": ");
+                var sensorParts = lineParts[0].Replace("Sensor at x=", "").Split(", y=");
+                var beaconParts = lineParts[1].Replace("closest beacon is at x=", "").Split(", y=");
+
+                var sensorCoords = (x: int.Parse(sensorParts[0]), y: int.Parse(sensorParts[1]));
+                var beaconCoords = (x: int.Parse(beaconParts[0]), y: int.Parse(beaconParts[1]));
+
+                grid[sensorCoords] = 'S'; //this will add the value if the key doesn't already exist, or update the existing value if it does
+                grid.TryAdd(beaconCoords, 'B');
+
+                MarkPositionsWithNoBeacon(grid, sensorCoords, beaconCoords);
+            }
+
+            return grid;
+        }
+
+        private static void MarkPositionsWithNoBeacon(Dictionary<(int x, int y), char> grid, (int x, int y) sensorCoords, (int x, int y) beaconCoords)
+        {
+            var manhattanDistance = Math.Abs(beaconCoords.x - sensorCoords.x) + Math.Abs(beaconCoords.y - sensorCoords.y);
+
+            var permutations = GetPermutations(manhattanDistance);
+
+            foreach (var (x, y) in permutations)
+            {
+                for (int i = sensorCoords.x + Math.Sign(x); Math.Abs(i - sensorCoords.x) <= Math.Abs(x); i += x == 0 ? 1 : Math.Sign(x))
+                {
+                    for (int j = sensorCoords.y + Math.Sign(y); Math.Abs(j - sensorCoords.y) <= Math.Abs(y); j += y == 0 ? 1 : Math.Sign(y))
+                    {
+                        grid.TryAdd((x: i, y: j), '#');
+                    }
+                }
+            }
+        }
+
+        private static HashSet<(int x, int y)> GetPermutations(int manhattanDistance)
+        {
+            var permutations = new HashSet<(int x, int y)>();
+
+            for (int i = 0; i <= manhattanDistance; i++)
+            {
+                permutations.Add((x: i, y: manhattanDistance - i));
+                permutations.Add((x: i, y: -(manhattanDistance - i)));
+                permutations.Add((x: -i, y: manhattanDistance - i));
+                permutations.Add((x: -i, y: -(manhattanDistance - i)));
+            }
+
+            return permutations;
+        }
+
+        public static int GetNumberOfPositionsWithoutBeacons(string[] input)
+        {
+            var grid = CreateGridFromInput(input);
+
+            return grid.Keys.Where(k => !grid[(k.x, k.y)].Equals('B')).Count();
+        }
+    }
+}
